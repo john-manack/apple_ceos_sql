ae96148edfdf887f78d242318ed0bafd93ec4985
John Manack <jmnck28@gmail.com>
2021-02-02 14:35:16 -0500

    removes changes sql file

--- changes.sql
+++
@@ -1,4 +0,0 @@
--- Updates a misspelling in a row by ID; it would also work to reference by another column besides ID
-UPDATE apple_ceos SET name = 'Steve Jobs' WHERE id = 15;
--- Deletes a row by ID
-DELETE FROM apple_ceos WHERE id = 17;
